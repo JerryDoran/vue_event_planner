5cf90857ba4b9539b54a45886807818f3c1aeb5a
Jerome Doran <disneyfan9184@gmail.com>
2020-02-07 15:47:22 -0500

    Set up navigation drawer

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,53 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+  <v-app>
+    <nav>
+      <v-app-bar app dark>
+        <v-app-bar-nav-icon @click="sideNav = !sideNav" class="hidden-sm-and-up"></v-app-bar-nav-icon>
+        <v-toolbar-title>
+          <h1 class="title grey--text">DevEvents</h1>
+        </v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-toolbar-items class="hidden-xs-only">
+          <v-btn text class="btn-nav grey--text">
+            <v-icon left>mdi-account</v-icon>View Events
+          </v-btn>
+        </v-toolbar-items>
+      </v-app-bar>
+
+      <!-- Navigation Drawer -->
+      <v-navigation-drawer v-model="sideNav" app class="drawer-color">
+        <v-list>
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon class="white--text">mdi-account</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title class="white--text">View Events</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+    </nav>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+// import HelloWorld from "./components/HelloWorld";
 
-#nav {
-  padding: 30px;
-}
+export default {
+  name: 'App',
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+  components: {},
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+  data: () => ({
+    sideNav: false
+  })
+};
+</script>
+
+<style scoped>
+.drawer-color {
+  background: #333 !important;
+}
+.btn-nav {
+  font-size: 11px !important;
 }
 </style>
